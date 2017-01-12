a7d46f22fb853febfcf798a721d8e55cff05e6f4
marcandy <marcandy.nj@gmail.com>
2017-01-12 12:34:57 -0700

    sql-auth

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const session = require('express-session')
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const config = require('./config.js')
+
+const app = express();
+
+app.use(session({
+  secret: config.secret,
+  resave: false,
+  saveUnitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use('facebook', new FacebookStrategy({
+  clientID: config.facebook.clientID,
+  clientSecret: config.facebook.clientSecret,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name']
+}, (accessToken, refreshToken, profile, done) => {
+  // access the database
+  done(null, profile);
+}))
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook'),
+(req, res) => {
+  // res.status(200).redirect('#'); // for demostration purpose we dont a home
+  res.status(200).send(req.user);
+});
+
+app.listen(3000, () => {
+  console.log('Listening 300');
+})
